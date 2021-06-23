de6f23c4ea1397520ca1912656888b383ba45bec
Dinesh Patil <dipatil@ptc.com>
2021-06-23 12:11:54 +0530

    Initial commit for fetching country list from JSON file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CountryCodeApp';
+  dataUrl = 'assets/data.json';
+  countries: any = [];
+
+  constructor(private httpClient: HttpClient){}
+  ngOnInit(){
+    this.httpClient.get(this.dataUrl).subscribe(data =>{
+      console.log(data);
+      this.countries = data;
+    })
+  }
 }
